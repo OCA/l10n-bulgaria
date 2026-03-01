6f23d2b948c577382a504f969d41f241ff70db89
rosenvladimirov <vladimirov.rosen@gmail.com>
2026-03-01 10:30:13 +0200

    [IMP] Remove `required=True` from NCOP Position field in hr_job model
    - Simplify the field definition by making it non-mandatory.

--- l10n_bg_payroll_classifications/models/hr_job.py
+++ l10n_bg_payroll_classifications/models/hr_job.py
@@ -10,7 +10,6 @@ class Job(models.Model):
         "bg.hr.payroll.ncop.classification",
         string="NCOP Position",
         help="NCOP Position",
-        required=True,
         ondelete="restrict",
     )
 
